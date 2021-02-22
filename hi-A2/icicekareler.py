3258bfeb8f6faf4dad7ea114c5c4f78006a1d2b3
serkancam <sekocam@gmail.com>
2021-02-22 20:12:32 +0300

    ders22022021

--- hi-A2/icicekareler.py
+++ hi-A2/icicekareler.py
@@ -4,10 +4,12 @@ t.Screen().setup(700,700)
 # https://studio.code.org/s/course4/stage/10/puzzle/4
 
 #çözüm
-for adim in range(15,301,15):
-    for i in range(4):
-        t.forward(adim)
-        t.left(90)
+t.pensize(5)
+t.dot()
+# for adim in range(15,301,15):
+#     for i in range(4):
+#         t.forward(adim)
+#         t.left(90)
 
 
 
--- hi-A2/test1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-cam  = cv2.VideoCapture(0)
-while True:
-	ret,img = cam.read()
-	cv2.imshow("yuz",img)
-	if cv2.waitKey(25)& 0xFF ==ord('q'):
-		cam.release()
-		cv2.destroyAllWindows()
